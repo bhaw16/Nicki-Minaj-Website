69abb52e5950f8d728894c33a2567f8d658a7eac
Brianna Hawkins <155006608+bhaw16@users.noreply.github.com>
2024-09-16 17:05:36 -0400

    updating button attributes and script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = () => {
-    var back_to_top_buttons = document.getElementsByClassName("back-to-top");
+    var back_to_top_buttons = Array.from(document.getElementsByClassName("back-to-top"));
+    back_to_top_buttons.push(document.getElementById("no-underline"));
     for (var i = 0; i < back_to_top_buttons.length; i++) {
         back_to_top_buttons[i].addEventListener("click", goToTop = () => {
             scrollTo(0, 0);
